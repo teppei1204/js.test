d0ad5231a4c8499dd3871bad2e018596fc5b391d
teppei1204 <m-n-m09@docomo.ne.jp>
2025-02-07 13:51:17 +0900

    Initial commit

---
+++ app.js
@@ -0,0 +1,62 @@
+// インデント
+// const bar = () => {
+//   const foo = 1 + 1;
+//   console.log(foo);
+// }
+
+// 配列
+// const onepice = ["赤犬","青きじ","黄猿"];
+
+// ループ文 while,map,forEach,do-whileなどなど
+// for (let index = 0; index < array.length; index++) {
+//   const element = array[index];
+// }
+// 初期値
+//let index = 0    ループを開始する際の値
+// 条件式
+// index < array.length    ループを継続するかどうかの条件
+// 増減式
+// index++    ループを繰り返す際に値をどれだけ増減させるかを表す
+
+// for(初期値; 条件式; 増減式){
+  // 処理
+// }
+
+// 条件式
+// let isLogin = false;
+// if(isLogin === false){
+//   alert("ログインしてくださいログインしてください")
+// }
+// if(isLogin)だけでもtrueの意味になる
+// if(!isLogin)でfalseの意味になる(論理否定)
+
+// if(foo > 0 && foo < 1000) {
+
+// }
+// &&  = かつ
+
+// if(foo === 0 || foo === 1){
+
+// }
+// ||  = または
+
+// const userType = "member";
+// if(userType === "member"){
+//   alert("アクセスできません");
+// } else if(userType === "admin"){
+//   alert("今から10分だけアクセスできます");
+// } else {
+//   console.log("アクセス完了");
+// }
+// せいぜい3つ4つにとどめておきましょう
+
+// Q.下記のコードを穴埋めし、「myMoneyが500以上ならisShippingでtrueを返す」ようにしなさい。必ずif文を使うこと
+
+// const myMoney = 1000;
+// let isShipping = false;
+// if(myMoney >= 500){
+//   isShipping = true;
+// } 
+// console.log(isShipping);
+
+
